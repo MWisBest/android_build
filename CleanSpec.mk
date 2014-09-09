7e9a2c7edbea561da359e2680c88e18fdf42e147
Dan Albert <danalbert@google.com>
2014-09-08 19:06:04 -0700

    Make gnu++11 the default standard version.
    There are a _lot_ of users of `typeof` in the tree, and they'll need a
    GNU flavored standard. Rather than fixing up all of these
    individually, let's just use gnu++11 as the default.
    
    Bug: 18466763
    Change-Id: Ia1bd3adaa74fadba259fda3d37653fb8d5cff69a

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -305,6 +305,11 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 # 5.0.2
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/build.prop)
 
+# Make GNU++11 the default standard version. This requires a cleanspec because
+# char16_t/char32_t will be real types now instead of typedefs, which means
+# an ABI change since the names will mangle differently.
+$(call add-clean-step, rm -rf $(OUT_DIR))
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
