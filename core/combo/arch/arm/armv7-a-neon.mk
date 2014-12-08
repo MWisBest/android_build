331b9702868830440c336077d3b380a97c83c45c
Kyle Repinski <repinski23@gmail.com>
2014-12-07 21:58:10 -0600

    Fix-ups to armv7-a-neon arch combo.

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -6,19 +6,21 @@ ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
+# If only -mcpu is specified, random -march flags could end up overriding it.
+# So, explicitly specify armv7-a to try and fix this where possible.
+arch_variant_cflags := -march=armv7-a
+
 ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a15
+	arch_variant_cflags += -mcpu=cortex-a15 -mtune=cortex-a15
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
-	arch_variant_cflags := -mcpu=cortex-a9
+	arch_variant_cflags += -mcpu=cortex-a9 -mtune=cortex-a9
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a8)
-	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_cflags += -mcpu=cortex-a8 -mtune=cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7
-else
-	arch_variant_cflags := -march=armv7-a
+	arch_variant_cflags += -mcpu=cortex-a7 -mtune=cortex-a7
 endif
 endif
 endif
@@ -38,10 +40,15 @@ ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)FPU_VARIANT)),neon-fp16)
 endif
 endif
 
-ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a8)
+# LTO can behave oddly if these aren't explicitly passed...
 arch_variant_ldflags := \
+	-Wl,-march=armv7-a \
+	-march=armv7-a
+
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a8)
+arch_variant_ldflags += \
 	-Wl,--fix-cortex-a8
 else
-arch_variant_ldflags := \
+arch_variant_ldflags += \
 	-Wl,--no-fix-cortex-a8
 endif
