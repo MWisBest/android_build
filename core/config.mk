f84a3ac00169079bae83bcb3f1d5e201a62e2658
Kyle Repinski <repinski23@gmail.com>
2015-01-30 12:08:29 -0600

    Disable global C++11 for now.

--- core/config.mk
+++ core/config.mk
@@ -117,7 +117,10 @@ SHOW_COMMANDS:= $(filter showcommands,$(MAKECMDGOALS))
 COMMON_GLOBAL_CFLAGS:= -DANDROID -fmessage-length=0 -W -Wall -Wno-unused -Winit-self -Wpointer-arith
 COMMON_RELEASE_CFLAGS:= -DNDEBUG -UDEBUG
 
-COMMON_GLOBAL_CPPFLAGS:= $(COMMON_GLOBAL_CFLAGS) -Wsign-promo -std=gnu++11
+COMMON_GLOBAL_CPPFLAGS:= $(COMMON_GLOBAL_CFLAGS) -Wsign-promo
+ifeq ($(DEBUG_FORCE_CXX11),true)
+COMMON_GLOBAL_CPPFLAGS+= -std=gnu++11
+endif
 COMMON_RELEASE_CPPFLAGS:= $(COMMON_RELEASE_CFLAGS)
 
 # Set the extensions used for various packages
