d38839b0d3c941813f89ee9cb760a137c44288cf
Kyle Repinski <repinski23@gmail.com>
2015-08-29 14:05:15 -0500

    build: arch-arm: Improve armv7-a-neon handling.
    Change-Id: I363640b6c270bca8ddf8037ea454255b3de17641

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -7,35 +7,46 @@ ARCH_ARM_HAVE_VFP               := true
 ARCH_ARM_HAVE_VFP_D32           := true
 ARCH_ARM_HAVE_NEON              := true
 
+# If only -mcpu is specified, random -march flags could end up overriding it.
+# So, explicitly specify armv7-a to try and fix this where possible.
+arch_variant_cflags := -march=armv7-a
+
 ifneq (,$(filter cortex-a15 krait denver,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-	arch_variant_cflags := -mcpu=cortex-a15
+	arch_variant_cflags += -mcpu=cortex-a15 -mtune=cortex-a15
+else
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
+	arch_variant_cflags += -mcpu=cortex-a9 -mtune=cortex-a9
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a8)
-	arch_variant_cflags := -mcpu=cortex-a8
+	arch_variant_cflags += -mcpu=cortex-a8 -mtune=cortex-a8
 else
 ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a7)
-	arch_variant_cflags := -mcpu=cortex-a7
-else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),cortex-a5)
-	arch_variant_cflags := -mcpu=cortex-a5
-else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),krait)
-	arch_variant_cflags := -mcpu=cortex-a9
-else
-ifeq ($(strip $(TARGET_CPU_VARIANT)),scorpion)
-	arch_variant_cflags := -mcpu=cortex-a8
-else
-	arch_variant_cflags := -march=armv7-a
+	arch_variant_cflags += -mcpu=cortex-a7 -mtune=cortex-a7
 endif
 endif
 endif
 endif
-endif
-endif
+
 
 arch_variant_cflags += \
-    -mfloat-abi=softfp \
-    -mfpu=neon
+	-mfloat-abi=softfp
 
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)FPU_VARIANT)),)
+arch_variant_cflags += \
+	-mfpu=neon
+else
+arch_variant_cflags += \
+	-mfpu=$(TARGET_$(combo_2nd_arch_prefix)FPU_VARIANT)
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)FPU_VARIANT)),neon-fp16)
+	ARCH_ARM_HAVE_NEON_FP16 := true
+endif
+endif
+
+
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a8)
 arch_variant_ldflags := \
 	-Wl,--fix-cortex-a8
+else
+arch_variant_ldflags := \
+	-Wl,--no-fix-cortex-a8
+endif
