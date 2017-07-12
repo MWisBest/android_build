f492bca31e66bed6870b1c0abebc5bb6cbfaf643
Kyle Repinski <repinski23@gmail.com>
2017-07-12 11:02:51 -0500

    releasetools: Tell blockimgdiff to shut up...

--- tools/releasetools/blockimgdiff.py
+++ tools/releasetools/blockimgdiff.py
@@ -697,17 +697,11 @@ class BlockImageDiff(object):
       for xf in self.transfers:
         if xf.style == "zero":
           tgt_size = xf.tgt_ranges.size() * self.tgt.blocksize
-          print("%10d %10d (%6.2f%%) %7s %s %s" % (
-              tgt_size, tgt_size, 100.0, xf.style, xf.tgt_name,
-              str(xf.tgt_ranges)))
 
         elif xf.style == "new":
           for piece in self.tgt.ReadRangeSet(xf.tgt_ranges):
             new_f.write(piece)
           tgt_size = xf.tgt_ranges.size() * self.tgt.blocksize
-          print("%10d %10d (%6.2f%%) %7s %s %s" % (
-              tgt_size, tgt_size, 100.0, xf.style,
-              xf.tgt_name, str(xf.tgt_ranges)))
 
         elif xf.style == "diff":
           src = self.src.ReadRangeSet(xf.src_ranges)
